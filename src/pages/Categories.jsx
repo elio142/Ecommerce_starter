f3618843335deef50e05d2c897147b1ac6a89c96
chko0 <chadi.kouzayha@gmail.com>
2025-04-23 23:58:58 +0300

    added Categories page (searching + filtering)

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { initialProducts } from "../lib/data";
+import Product from "../components/Product";
 
 export default function Categories() {
     const [query, setQuery] = useState("");
     const [minPrice, setMinPrice] = useState(1);
     const [maxPrice, setMaxPrice] = useState(1000);
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    const filteredProducts = initialProducts.filter((product) => {
+        const titleMatch = product.name.toLowerCase().includes(query.toLowerCase());
+        const descriptionMatch = product.description.toLowerCase().includes(query.toLowerCase());
+        const priceMatch = product.price >= minPrice && product.price <= maxPrice;
+        const categoryMatch = (selectedCategory) ? (product.category == selectedCategory) : true;
+        return (titleMatch || descriptionMatch) && priceMatch && categoryMatch;
+    });
+
+    // useEffect(() => {
+    //     function fetchProducts() {
+
+    //     }
+
+    //     fetchProducts();
+    // }, [query]);
+
 
     return <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="mb-8">
@@ -11,7 +31,11 @@ export default function Categories() {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 bg-white p-6 rounded-lg shadow-sm">
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-2">Category</label>
-                    <select className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500">
+                    <select
+                        className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                        value={selectedCategory}
+                        onChange={(e) => setSelectedCategory(e.target.value)}
+                    >
                         <option value="">All Categories</option>
                         <option value="Electronics">Electronics</option>
                         <option value="Sports">Sports</option>
@@ -35,6 +59,14 @@ export default function Categories() {
             </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        </div></div>
+            {filteredProducts.length > 0 ? (
+                filteredProducts.map(product => (
+                    <Product key={product.id} id={product.id} />
+                ))
+            ) : (
+                <></>
+            )}
+        </div>
+    </div>
 
 }
