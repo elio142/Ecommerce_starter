a77407daa5ecfc6bb19b511b352b365898114d58
chko0 <chadi.kouzayha@gmail.com>
2025-04-27 23:05:48 +0300

    added imports in preparation of merging

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,28 @@ import Home from "./pages/home"
 import { Routes, Route } from "react-router-dom";
 import Favorites from "./pages/Favorites";
 import Cart from "./pages/Cart";
+import AuthProvider from "./context/AuthProvider";
 import CartProvider from "./context/CartContext";
 import Checkout from "./pages/Checkout";
+
 function App() {
   return (
     <div>
-      <CartProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Categories" element={<Categories />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/Checkout" element={<Checkout />} />
-        </Routes>
-        <Footer />
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Categories" element={<Categories />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/Checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </CartProvider>
+      </AuthProvider>
     </div>
   );
 }
